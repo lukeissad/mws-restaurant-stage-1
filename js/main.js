fdef4af3d20f4b3cad0f8703ead37c9b84257a63
Lucas Sadzik <ritsuboshi@gmail.com>
2018-07-05 16:33:00 +0200

    Update main.js to add button role to <a>

--- js/main.js
+++ js/main.js
@@ -180,6 +180,7 @@ createRestaurantHTML = (restaurant) => {
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   more.setAttribute('aria-label', restaurant.name + ' Details');
+  more.setAttribute('role', 'button');
   li.append(more)
 
   return li
