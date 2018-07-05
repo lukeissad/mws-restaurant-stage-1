de6a4c442b2e2982a6a3ff425ff123744dc50a39
Lucas Sadzik <ritsuboshi@gmail.com>
2018-07-05 21:17:39 +0200

    Move Service Worker registration to js/dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,6 +1,14 @@
 /**
  * Common database helper functions.
  */
+
+// Service Worker registration.
+navigator.serviceWorker.register('/sw.js').then(function() {
+  console.log('Service Worker registered!');
+}).catch(function() {
+  console.log('Registration failed');
+});
+
 class DBHelper {
 
   /**
@@ -157,7 +165,7 @@ class DBHelper {
    * Map marker for a restaurant.
    */
    static mapMarkerForRestaurant(restaurant, map) {
-    // https://leafletjs.com/reference-1.3.0.html#marker  
+    // https://leafletjs.com/reference-1.3.0.html#marker
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
       {title: restaurant.name,
       alt: restaurant.name,
@@ -165,7 +173,7 @@ class DBHelper {
       })
       marker.addTo(newMap);
     return marker;
-  } 
+  }
   /* static mapMarkerForRestaurant(restaurant, map) {
     const marker = new google.maps.Marker({
       position: restaurant.latlng,
@@ -178,4 +186,3 @@ class DBHelper {
   } */
 
 }
-
